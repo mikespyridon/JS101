a818433d6d4db20f256a553c748e6930bc17f671
Mike Spyridon <mikespyridon@gmail.com>
2023-02-23 18:29:34 +0000

    calculator bonus steps

---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,62 @@
+// A function that returns the sum of two numbers:
+//
+// Given two numbers
+//
+// Declare two arugments, inside the function return the sum of the two arguments by adding them together 
+//
+// START
+//
+// Declare a function sumOfTwoNumbers(num1, num2) 
+// return num1 + num2
+//
+// END
+//
+// // A function that takes an array of strings, and returns a string that is all those strings concatenated together
+//
+// Given collection of strings called "strings"
+//
+// declare a function called concatStrings with one argument 
+// 
+// Use the 'join' method on the argument with a empty string as an argment inside the join method
+//
+// retun the argument of the concatStrings function 
+//
+// START
+//
+//  Given a collection of strings called 'strings'
+//
+// Declare a function called concatStrings(array)
+//
+//   return array.join('');
+//
+// PRINT concatStrings(strings);
+//
+// END
+// // a method that returns a new array of every other element of original array
+//
+// Given a collection of numbers
+//
+// Iterate through the collection one by one 
+//   - Use the filter array method 
+//   - return the index moduolo 2 = 0 
+//   - print the new array
+//
+// START
+//
+// given a collection of numbers named arr
+//
+// SET x = arr.filter((element, index) => {
+//   return index % 2 === 0;
+// });
+//
+// PRINT x
+//
+// END
+//
+//
+// // 
+//
+//
+//
+//
+//
